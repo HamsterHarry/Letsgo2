7c8a2501a4e37b2578552fcee6a29cdefd79e384
HamsterHarry <nathan.conley@pcds.org>
2023-03-10 10:31:08 -0700

    adding bootstrap

---
+++ Menu.js
@@ -0,0 +1,10 @@
+function Menu() {
+    return (
+      <div className="Menu">
+        <Menu />
+       <h3> Menu </h3>
+      </div>
+    );
+  }
+  
+  export default Menu;
--- src/App.js
+++ src/App.js
@@ -1,9 +1,16 @@
 import logo from './logo.svg';
 import './App.css';
+import Menu from './Menu.js';
+import Button from 'react-bootstrap/Button';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
 
 function App() {
   return (
     <div className="App">
+      <Menu />
+      <Button> Bootrstrap Button </Button>
+      <button className="btn btn-success">sort of a buttoneer</button>
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
